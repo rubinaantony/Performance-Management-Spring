e433c11ab435442195576273a6f6bbffaec081c3
rubinaantony <rubina95.antony@gmail.com>
2020-06-22 22:31:53 +0530

    update find function for review

--- src/main/java/com/example/demo/repository/MessageRepo.java
+++ src/main/java/com/example/demo/repository/MessageRepo.java
@@ -1,10 +1,15 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.bean.Message;
 
 @Repository
 public interface MessageRepo extends JpaRepository<Message,Integer>{
+	 @Query("select c from Message c where c.m_reviewing = :m_reviewing")
+	List<Message> findByM_reviewing(String m_reviewing);
 }
